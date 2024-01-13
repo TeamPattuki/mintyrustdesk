5ff39c913035283a5e176e4f9b43282f3b0808a5
TeamPattuki <42864522+TeamPattuki@users.noreply.github.com>
2024-01-13 12:07:22 +0800

    Update lang.rs

--- src/lang.rs
+++ src/lang.rs
@@ -152,7 +152,7 @@ pub fn translate_locale(name: String, locale: &str) -> String {
     if let Some(v) = m.get(&name as &str) {
         if v.is_empty() {
             if lang != "en" {
-                if let Some(v) = en::T.get(&name as &str) {
+                if let Some(v) = ko::T.get(&name as &str) {
                     return replace(v);
                 }
             }
