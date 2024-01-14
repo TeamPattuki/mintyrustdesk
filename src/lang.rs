82f39d5bbd2f1ac93e61e1199e11bc1549b6a91e
TeamPattuki <42864522+TeamPattuki@users.noreply.github.com>
2024-01-14 14:11:29 +0800

    Update lang.rs

--- src/lang.rs
+++ src/lang.rs
@@ -138,7 +138,7 @@ pub fn translate_locale(name: String, locale: &str) -> String {
         "lt" => lt::T.deref(),
         "lv" => lv::T.deref(),
         "ar" => ar::T.deref(),
-        "en" => en::T.deref(),
+        "ko" => ko::T.deref(),
         _ => ko::T.deref(),
     };
     let (name, placeholder_value) = extract_placeholder(&name);
@@ -152,7 +152,7 @@ pub fn translate_locale(name: String, locale: &str) -> String {
     if let Some(v) = m.get(&name as &str) {
         if v.is_empty() {
             if lang != "ko" {
-                if let Some(v) = ko::T.get(&name as &str) {
+                if let Some(v) = en::T.get(&name as &str) {
                     return replace(v);
                 }
             }
